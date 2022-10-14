a506c8658175756f7f89c99b9317cc7cf16da816
Mibei Collins <mibeicollins07@gmail.com>
2022-10-14 11:57:22 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let num1 = 5;
+let num2 = 6;
+let largest;
+
+if(num1 >= num2) {
+  largest = num1;
+}
+
+else if(num2 >= num1) {
+   largest = num2;
+}
+
+console.log("The largest number is:" +largest);
